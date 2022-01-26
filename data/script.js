a41a07290d421804947c192b952cf86c7758b919
Fredrik Jonson <freddemorta@gmail.com>
2022-01-26 14:51:37 +0100

    initial

--- data/script.js
+++ data/script.js
@@ -1,24 +1,19 @@
-const container = document.querySelector('.container');
-const mainContainer = document.createElement('div');
-mainContainer.classList.add('canvas-container');
-container.appendChild(mainContainer)
+const container = document.querySelector(".container");
+const mainContainer = document.createElement("div");
+mainContainer.classList.add("canvas-container");
+container.appendChild(mainContainer);
 
+const DEFAULT_COLOR = "black";
+const DEFAULT_BACKGROUND = "#fefefe";
 
-
-
-const DEFAULT_COLOR = 'black';
-const DEFAULT_BACKGROUND = '#fefefe';
-
-
-const size = 100;
+const size = 50;
 let currentColor = DEFAULT_COLOR;
 let backgroundColor = DEFAULT_BACKGROUND;
 
-
-const colorPicker = document.getElementById('colorPicker');
-const resetButton = document.getElementById('reset');
-const ereaserButton = document.getElementById('eraser');
-const paintButton = document.getElementById('paint');
+const colorPicker = document.getElementById("colorPicker");
+const resetButton = document.getElementById("reset");
+const ereaserButton = document.getElementById("eraser");
+const paintButton = document.getElementById("paint");
 
 colorPicker.onchange = (e) => setCurrentColor(e.target.value);
 paintButton.onclick = () => setCurrentColor(DEFAULT_COLOR);
@@ -26,34 +21,31 @@ resetButton.onclick = () => reset();
 ereaserButton.onclick = () => setCurrentColor(backgroundColor);
 
 for (let index = 0; index < size; index++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+  const row = document.createElement("div");
+  row.classList.add("row");
 
-    for (let index = 0; index < size; index++) {
-        const col = document.createElement('div');
-        col.classList.add('cell');
-        col.style.backgroundColor = backgroundColor;
-        row.appendChild(col);
-    };
-    mainContainer.appendChild(row);
-};
+  for (let index = 0; index < size; index++) {
+    const col = document.createElement("div");
+    col.classList.add("cell");
+    col.style.backgroundColor = backgroundColor;
+    row.appendChild(col);
+  }
+  mainContainer.appendChild(row);
+}
 
-
-const col = document.querySelectorAll('.cell');
-col.forEach(col => col.addEventListener('mouseover', function (e) {
+const col = document.querySelectorAll(".cell");
+col.forEach((col) =>
+  col.addEventListener("mouseover", function (e) {
     e.target.style.background = currentColor;
-}));
-
+  })
+);
 
 function reset() {
-    col.forEach((pixel) => {
-        pixel.style.background = backgroundColor;
-    })
+  col.forEach((pixel) => {
+    pixel.style.background = backgroundColor;
+  });
 }
 
 function setCurrentColor(newColor) {
-    currentColor = newColor;
+  currentColor = newColor;
 }
-
-
-
